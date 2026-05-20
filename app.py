fe7765c63a406a54033346f9f4a04f940ad3ccf5
JulioAnticona <julioanticona@gmail.com>
2026-05-20 12:38:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,13 @@ if sesion == "Sesión 1":
 if sesion == "Sesión 2":
   st.write("Bienvenido a la sesión 2")
 
+  st.number_input("Ingrese el precio del producto")
+  descuento = st.number_input("Ingrese el descuento del producto")
+
+  precio_final_producto = precio - (precio*descuento)
+
+  st.write ("El precio del producto es:" , precio_final_producto)
+
 if sesion == "Sesión 3":
   st.write("Bienvenido a la sesión 3")
 

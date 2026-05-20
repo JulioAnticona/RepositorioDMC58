f29014c2ff3f5b91efac6a3814d167f49d1a4ba1
JulioAnticona <julioanticona@gmail.com>
2026-05-20 12:14:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,5 @@ st.title("Mi primera aplicacion en python")
 st.sidebar.title("Parametros")
 
 st.write("Elaborado por:Julio Anticona")
+
+Sesion = st.selectbox("Selecione una sesión",[ "Sesión 1",Sesión 2, Sesión 3, Sesión 4])

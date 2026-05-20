246b071057cf05801512f3fd33088b50acc837ee
JulioAnticona <julioanticona@gmail.com>
2026-05-20 12:20:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ st.sidebar.title("Parametros")
 
 st.write("Elaborado por:Julio Anticona")
 
-sesion = st.siderbar.selectbox("Selecione una sesión",[ "Sesión 1","Sesión 2", "Sesión 3", "Sesión 4"])
+sesion = st.sidebar.selectbox("Selecione una sesión",[ "Sesión 1","Sesión 2", "Sesión 3", "Sesión 4"])

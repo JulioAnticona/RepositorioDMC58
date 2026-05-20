2359c059139e9899d67768484227168803447192
JulioAnticona <julioanticona@gmail.com>
2026-05-20 12:48:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,10 +17,10 @@ if sesion == "Sesión 1":
 if sesion == "Sesión 2":
   st.write("Bienvenido a la sesión 2")
 
-  precio= st.number_input("Ingrese el precio del producto")
-  descuento = st.number_input("Ingrese el descuento del producto")
+  precio= st.number_input("Ingrese el precio del producto",min_value=0, max_valu=5000 , value =1200)
+  descuento = st.number_input("Ingrese el descuento del producto del 0 al 100%", min_value=0, max_valu=100)
 
-  precio_final_producto = precio - (precio*descuento)
+  precio_final_producto = precio - (precio*descuento/100)
 
   st.write ("El precio del producto es:" , precio_final_producto)
 

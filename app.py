b6d0311704128893fb84e5c0ef7cbce6ce752afc
JulioAnticona <julioanticona@gmail.com>
2026-05-20 12:24:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,15 @@ st.sidebar.title("Parametros")
 st.write("Elaborado por:Julio Anticona")
 
 sesion = st.sidebar.selectbox("Selecione una sesión",[ "Sesión 1","Sesión 2", "Sesión 3", "Sesión 4"])
+
+if sesion == "Sesión 1":
+  st.write("Bienvenido a la sesión 1")
+
+if sesion == "Sesión 2":
+  st.write("Bienvenido a la sesión 2")
+
+if sesion == "Sesión 3":
+  st.write("Bienvenido a la sesión 3")
+
+if sesion == "Sesión 4":
+  st.write("Bienvenido a la sesión 4")
